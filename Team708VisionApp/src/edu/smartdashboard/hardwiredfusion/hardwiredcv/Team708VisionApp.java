5ce7319a06bb599d9ef6edb33e4c304d717d4d42
Robotics <Robotics@HS-ROBOLT-01.admin.hatboro-horsham.org>
2013-01-27 16:57:29 -0500

    1/27/13 - Finished SmartDashboard vision app - added reticule - added display of RoboRealm bounding box around target.

--- Team708VisionApp/src/edu/smartdashboard/hardwiredfusion/hardwiredcv/Team708VisionApp.java
+++ Team708VisionApp/src/edu/smartdashboard/hardwiredfusion/hardwiredcv/Team708VisionApp.java
@@ -1,17 +1,17 @@
 package edu.smartdashboard.hardwiredfusion.hardwiredcv;
 
 
-import com.googlecode.javacv.cpp.opencv_core;
-import com.googlecode.javacv.cpp.opencv_core.CvSize;
-import com.googlecode.javacv.cpp.opencv_core.IplImage;
-import com.googlecode.javacv.cpp.opencv_imgproc;
 import edu.wpi.first.smartdashboard.camera.WPICameraExtension;
-import edu.wpi.first.wpijavacv.CVUtils;
+import edu.wpi.first.smartdashboard.robot.Robot;
 import edu.wpi.first.wpijavacv.WPIColor;
 import edu.wpi.first.wpijavacv.WPIColorImage;
 import edu.wpi.first.wpijavacv.WPIImage;
 import edu.wpi.first.wpijavacv.WPIPoint;
-import java.awt.image.Raster;
+import edu.wpi.first.wpilibj.tables.ITable;
+import edu.wpi.first.wpilibj.tables.TableKeyNotDefinedException;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /*
  * To change this template, choose Tools | Templates
@@ -19,70 +19,74 @@ import java.awt.image.Raster;
  */
 
 /**
- *
- * @author Robotics
+ * This SmartDashboard extension accepts data being output by the
+ * RoboRealm vision processing program and displays it as a
+ * box around the acquired target.
+ * 
+ * p1 = top left corner of box
+ * p2 = top right
+ * p3 = bottom left
+ * p4 = bottom right
+ * @author Connor Willison
  */
 public class Team708VisionApp extends WPICameraExtension{
-
-    private IplImage rawCVFormat;
-    private CvSize imageDims = opencv_core.cvSize(320,240);
-    private IplImage hsvImage = IplImage.create(imageDims,8,3);
-    private IplImage hue = IplImage.create(imageDims,8,1);
-    private IplImage saturation = IplImage.create(imageDims,8,1);
-    private IplImage value = IplImage.create(imageDims, 8, 1);
-    private IplImage lowerSide = IplImage.create(imageDims, 8,1);
-    private IplImage upperSide = IplImage.create(imageDims, 8,1);
-    private Raster r;
-    private int[] pixel = new int[1];
     
-    //color filtering constants
-    private final int hueLow = 95;
-    private final int hueHigh = 151;
-    private final int satLow = 205;
-    private final int valLow = 205;
+    private int p1x,p1y,p2x,p3y;
+    private boolean written = false;
+    private boolean initialized = false;
+    private ITable robot;
+    private WPIPoint rp1,rp2,rp3,rp4;
+    private int reticuleRadius = 5;
     
-    @Override
     public WPIImage processImage(WPIColorImage rawImage) {
-        rawCVFormat = CVUtils.getIplImage(rawImage);
         
-        // convert raw image to HSV color space
-        opencv_imgproc.cvCvtColor(rawCVFormat,hsvImage, opencv_imgproc.CV_BGR2HSV);
-        
-        //split each HSV channel into a separate image
-        opencv_core.cvSplit(hsvImage,hue,saturation,value, null);
-        
-        //filter out all pixels except those orange colored (retroreflective tape)
-        //that is, between 23 and 62 on hue channel.
-        opencv_imgproc.cvThreshold(hue,upperSide,hueLow,255,opencv_imgproc.CV_THRESH_BINARY);
-        opencv_imgproc.cvThreshold(hue,lowerSide,hueHigh,255,opencv_imgproc.CV_THRESH_BINARY_INV);
-        
-        //filter out saturation values that are too low
-        opencv_imgproc.cvThreshold(saturation,saturation,satLow,255,opencv_imgproc.CV_THRESH_BINARY);
-        
-        //filter out saturation values that are too low
-        opencv_imgproc.cvThreshold(value,value,valLow,255,opencv_imgproc.CV_THRESH_BINARY);
-        
-        //combine the images containing the pixels that we want
-        opencv_core.cvAnd(hue,lowerSide,hue,null);
-        opencv_core.cvAnd(hue,upperSide,hue,null);
-        opencv_core.cvAnd(hue,saturation,hue,null);
-        opencv_core.cvAnd(hue,value,hue,null);
-        
-        r = hue.getBufferedImage().getData();
-        
-        for(int x = 0; x < r.getWidth(); x++)
+        if(!initialized)
         {
-            for(int y = 0; y < r.getHeight(); y++)
+            rp1 = new WPIPoint(rawImage.getWidth()/2,rawImage.getHeight()/2 - reticuleRadius);
+            rp2 = new WPIPoint(rawImage.getWidth()/2,rawImage.getHeight()/2 + reticuleRadius);
+            
+            rp3 = new WPIPoint(rawImage.getWidth()/2 - reticuleRadius,rawImage.getHeight()/2);
+            rp4 = new WPIPoint(rawImage.getWidth()/2 + reticuleRadius,rawImage.getHeight()/2);
+            
+            initialized = true;
+        }
+//        
+	try
+	{
+            robot = Robot.getTable("vision");
+            
+            p1x = (int)robot.getNumber("p1x");
+            p1y = rawImage.getHeight() - (int)robot.getNumber("p1y");
+            p2x = (int)robot.getNumber("p2x");
+            p3y = rawImage.getHeight() - (int)robot.getNumber("p3y");
+            
+            //draw a box around the acquired target
+            rawImage.drawRect(p1x,p1y,p2x - p1x, p3y - p1y, WPIColor.GREEN,1);
+            
+            //draw a reticule in the center of the screen for easier aiming
+            rawImage.drawLine(rp1,rp2, WPIColor.GREEN, 1);
+            rawImage.drawLine(rp3,rp4, WPIColor.GREEN, 1);
+	}
+	catch (TableKeyNotDefinedException exp)
+        {
+            if(!written)
             {
-                r.getPixel(x,y,pixel);
-                if(pixel[0] > 0)
+                try{
+                    File f = new File("C:/Users/Robotics/Desktop/dash_error.txt");
+                    if(!f.exists()) f.createNewFile();
+                    PrintStream stream = new PrintStream(f);
+                    exp.printStackTrace(stream);
+                    stream.close();
+                    
+                }catch(IOException e)
                 {
-                    rawImage.drawPoint(new WPIPoint(x,y), WPIColor.GREEN,1);
                     
                 }
+                
+                written = true;
             }
-        }
-        
+	}
+
         return rawImage;
     }
     
